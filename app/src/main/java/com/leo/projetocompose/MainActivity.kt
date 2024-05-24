d2dd52ded9ea4c01b4e1ac0f79858948c0825679
WIN11 - Leonardo <leonardolopbraga@gmail.com>
2024-05-24 18:59:35 -0300

    Perfil com borda + Botão switch com cores personalizadas

--- app/src/main/java/com/leo/projetocompose/MainActivity.kt
+++ app/src/main/java/com/leo/projetocompose/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.leo.projetocompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -16,10 +20,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.mutableStateOf
@@ -28,7 +36,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.leo.projetocompose.ui.theme.HotPink
+import com.leo.projetocompose.ui.theme.Magenta
 import com.leo.projetocompose.ui.theme.ProjetoComposeTheme
+import com.leo.projetocompose.ui.theme.Purple
 
 class MainActivity() : ComponentActivity() {
 
@@ -62,43 +73,82 @@ class MainActivity() : ComponentActivity() {
 
     @Composable
     fun PrimeiraTela(isDarkTheme: Boolean, onToggleDarkMode: (Boolean) -> Unit){
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize(),
-            //verticalAlignment = Alignment.CenterVertically
-            horizontalAlignment = Alignment.CenterHorizontally
+            contentAlignment = Alignment.Center
+        ){
+            Column() {
+                Column(
+                    modifier = Modifier
+                        .padding(all = 8.dp),
+                    //verticalAlignment = Alignment.CenterVertically
+                    horizontalAlignment = Alignment.CenterHorizontally
 
-        ) {
-            Spacer(modifier = Modifier.height(64.dp))
+                ) {
+                    //Spacer(modifier = Modifier.height(32.dp))
 
-            Image(
-                painter = painterResource(R.drawable.eu),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(200.dp)
-                    .aspectRatio(0.8f)
-                    .clip(RoundedCornerShape(50))
-            )
-            Spacer(modifier = Modifier.height(48.dp))
+                    Image(
+                        painter = painterResource(R.drawable.elana),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(288.dp)
+                            .clip(CircleShape)
+                            .border(1.44.dp, Color.Magenta, CircleShape)
+                    )
+                    //Spacer(modifier = Modifier.width(8.dp))
+                    Spacer(modifier = Modifier.height(32.dp))
 
-            Column {
-                Text(
-                    text = "Hello World!",
-                    color = Color.Green,
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = "Hello World!",
+                            color =  MaterialTheme.colorScheme.secondary,
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(horizontal = 8.dp)
 
-                )
-                Text(
-                    text = "Leonardo Lopes",
-                    color = Color.Yellow,
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                )
-                Switch(checked = isDarkTheme, onCheckedChange = onToggleDarkMode)
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+
+                        Surface(shape = MaterialTheme.shapes.large, shadowElevation = 2.dp) {
+                            Text(
+                                text = "Elana Trindade",
+                                color = MaterialTheme.colorScheme.primary,
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(32.dp))
+
+                        Switch(
+                            checked = isDarkTheme,
+                            onCheckedChange = onToggleDarkMode,
+                            colors = SwitchDefaults.colors(
+                                //Cor do botão ligado
+                                checkedThumbColor = Magenta,
+                                //Cor do background com o botão ligado
+                                checkedTrackColor = Color.White.copy(alpha = 0.8f),
+                                //Cor da borda com o botão ligado
+                                checkedBorderColor = Magenta,
+                                //????????
+                                checkedIconColor = MaterialTheme.colorScheme.onSecondary,
+
+                                //Cor do botão desligado
+                                uncheckedThumbColor = Purple,
+                                //Cor do background com o botão desligado
+                                uncheckedTrackColor = Color.Black.copy(alpha = 0.2f),
+                                //Cor da borda com o botão desligado
+                                uncheckedBorderColor = Purple,
+                                //????????
+                                uncheckedIconColor = MaterialTheme.colorScheme.onSecondary
+                            )
+                        )
+                    }
+                }
             }
         }
     }
 
-    @Preview(showBackground = true)
+    @Preview(showBackground = false, uiMode = Configuration.UI_MODE_NIGHT_YES)
     @Composable
     fun PreviewPrimeiraTela() {
         ProjetoComposeTheme {
--- app/src/main/java/com/leo/projetocompose/ui/theme/Color.kt
+++ app/src/main/java/com/leo/projetocompose/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package com.leo.projetocompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Magenta = Color(0xFFFF00FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Mode Colors
+val DarkPink = Color(0xFFFF1493)
+val DarkOrchid = Color(0xFF9932CC)
+val DarkMagenta = Color(0xFF8B008B)
+val BlueViolet = Color(0xFF8A2BE2)
+val DarkViolet = Color(0xFF9400D3)
+val Purple = Color(0xFF800080)
+val ElectricPurple = Color(0xFFBF00FF)
+val DeepPurple = Color(0xFF512DA8)
+val MidnightBlue = Color(0xFF191970)
+
+// Light Mode Colors
+val Pink = Color(0xFFFFC0CB)
+val LightPink = Color(0xFFFFB6C1)
+val HotPink = Color(0xFFFF69B4)
+val Lavender = Color(0xFFE6E6FA)
+val Thistle = Color(0xFFD8BFD8)
+val Purpura = Color(0xFFDB7093)
+val Violet = Color(0xFFEE82EE)
+val Orchid = Color(0xFFBA55D3)
+val LightPurple = Color(0xFF9370DB)
+val SoftLavender = Color(0xFFF3E5F5)
+val LightBlue = Color(0xFFADD8E6)
